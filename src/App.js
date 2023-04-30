cb680b776a427e97de1bc37e80af68dec34eb879
Mustafa Assaf <mustafakassaf@gmail.com>
2023-04-30 21:26:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to cloud course
+          Welcome to cloud course.    :)
         </p>
       </header>
     </div>
